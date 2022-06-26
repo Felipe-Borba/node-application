8ed758297b5ed99c0829a7c8d01c7f8853729af1
Felipe Borba <felipe.borba0109@gmail.com>
2022-06-26 17:11:17 -0300

    handle errors

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if(!name) {
+      throw new AppError("nome é obrigatório!")
+    }
 
     response.json({ ok: true, name, email, password });
   }
