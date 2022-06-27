7736c22302c41aabdec9278156df79196b4f29d6
Felipe Borba <felipe.borba0109@gmail.com>
2022-06-26 22:58:05 -0300

    add simple migration

--- src/server.js
+++ src/server.js
@@ -2,12 +2,12 @@ require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
 const AppError = require("./utils/AppError");
-const database = require("./database/sqlite");
+const migrationsRun = require("./database/sqlite/migrations");
 
 const app = express();
 app.use(express.json());
 app.use(routes);
-database();
+migrationsRun();
 
 app.use((error, request, response, next) => {
   if (error instanceof AppError) {
